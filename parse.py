cfb140be1d2dca101f3fb2dcbb315c1aa15c4002
Connor Kelly <connor.r.kelly@gmail.com>
2012-11-01 04:48:15 -0400

    Create parse.py
    Responsible for parsing through configuration files and directories.

---
+++ parse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+####
+#	importing python modules
+####
+import ConfigParser, os
+
+####
+#	importing strmr modules
+####
+import music
+
+class config:
+	def __init__(self):
+		pass

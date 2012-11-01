af660bcb41f10ff29aad8da16605bc83f22b0c6a
Connor Kelly <connor.r.kelly@gmail.com>
2012-11-01 04:50:59 -0400

    Create __init__ functions
    __init__ functions can accept information for the whole class: done for
    pulling information from file.  Pulling from the file allows information
    to be populated quickly without looking through actual files.  Directory
    is walked in the background to check for changes.

--- music.py
+++ music.py
@@ -3,13 +3,30 @@
 import hashlib
 
 class song:
-	def __init__(self):
-		pass
+	def __init__(self, name="", id="", path="", filename="", hash=""):
+		self.id = id
+		self.name = name
+		self.path = path
+		self.filename = filename
+		self.hash = hash
 
 class artist:
-	def __init__(self):
-		pass
+	def __init__(self, name="", id=""):
+		"""
+			artist class that has an artist ID, the artist name, and an array
+			of album objects
+		"""
+		self.id = ""
+		self.albums = []
+		self.name = name
 
 class album:
-	def __init__(self):
-		pass
+	def __init__(self, name="", year="", artist=""):
+		"""
+			album class which has the name of the album, year it came out
+			and the artist that wrote it.
+		"""
+		self.id = ""
+		self.name = name
+		self.songs = []
+		self.year = year
